52c6e4a3d61c6c273207947ccdfff3685b9132cf
VimanyuAgg <vimanyu1022007@gmail.com>
2017-03-11 21:08:42 -0800

    imported java parser dependencies in SeqDiagramGenerator file

--- src/main/java/cmpe202/umlparser/SeqDiagramGenerator.java
+++ src/main/java/cmpe202/umlparser/SeqDiagramGenerator.java
@@ -1,5 +1,14 @@
 package cmpe202.umlparser;
 
+import java.io.*;
+import java.util.*;
+
+import com.github.javaparser.*;
+import com.github.javaparser.ast.*;
+import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.stmt.*;
+
 class SeqDiagramGenerator{
 	
 	
