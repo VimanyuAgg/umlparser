adef9802d9f9c2ebf2e0b0cb085394d3ee8222ce
VimanyuAgg <vimanyu1022007@gmail.com>
2017-03-18 12:08:33 -0700

    Added a check to validate user input for arguments

--- src/main/java/cmpe202/umlparser/App.java
+++ src/main/java/cmpe202/umlparser/App.java
@@ -1,14 +1,18 @@
 package cmpe202.umlparser;
 
 class App{
-	
+
 	public static void main(String[] args){
+		if(args.length !=2){
+			System.out.println("Error: please enter valid arguments");
+			System.exit(0);
+		}
 		parser myUmlParser = new parser(args[1],args[2]);
 		myUmlParser.startParsing();
-		
-		
+
+
 	}
-	
-	
-	
+
+
+
 }
