cd14ec47ab49d54136e4b8fbc6170210af480812
VimanyuAgg <vimanyu1022007@gmail.com>
2017-03-18 12:22:30 -0700

    Added javadoc to parser.java

--- src/main/java/cmpe202/umlparser/parser.java
+++ src/main/java/cmpe202/umlparser/parser.java
@@ -1,4 +1,8 @@
 package cmpe202.umlparser;
+ /**
+  * Parser takes the input as per the given requirements
+  * It depends on SeqDiagramGenerator.java to actually create Sequence Diagrams
+  */
 
 import com.github.javaparser.*;
 import com.github.javaparser.ast.*;
@@ -6,19 +10,24 @@ import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 
 public class parser{
-	
+
 	String sourceFilePath;
 	String destFilePath;
-	
+		ArrayList<CompilationUnit> compilationArray;
+
 	parser(String sourcePath, String destPath){
 		this.sourceFilePath = sourcePath;
 		this.destFilePath = sourceFilePath + "\\" + destPath + ".jpeg";
-		
+
 	}
-	
-	public void startParsing(){
-		
+
+	public void startParsing() throws Exception{
+
 		//Dummy method created
+
+		compilationArray = getArray(sourceFilePath);
+		createDiag(compilation)
+
 	}
-	
+
 }
