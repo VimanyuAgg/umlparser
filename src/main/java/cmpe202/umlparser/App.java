c23cfc9ce43a3fc83e691a2e4a278f6ec68eb0d2
VimanyuAgg <vimanyu1022007@gmail.com>
2017-04-01 16:56:24 -0700

    Handled invalid arg case in app.java

--- src/main/java/cmpe202/umlparser/App.java
+++ src/main/java/cmpe202/umlparser/App.java
@@ -1,22 +1,26 @@
 package cmpe202.umlparser;
 /*
  * App.java contains the main method which drives the program
- * This takes input as two arguments as per the specifications in readme 
- * 
+ * This takes input as two arguments as per the specifications in readme
+ *
  */
 class App{
-	
+
 	public static void main(String[] args){
 		if(args.length !=2){
 			System.out.println("Error: please enter valid arguments");
 			System.exit(0);
 		}
-		parser myUmlParser = new parser(args[1],args[2]);
-		myUmlParser.startParsing();
-		
-		
+		else if (args[0].equals(("sqnc")))
+		{parser myUmlParser = new parser(args[1],args[2]);
+			myUmlParser.startParsing();
+		}
+		else{
+			System.out.println("Invalid parameter in arguments");
+		}
 	}
-	
-	
-	
+
+
+
+
 }
