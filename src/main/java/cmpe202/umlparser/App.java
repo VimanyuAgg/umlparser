152b2c1757f08b22413291da9a2ee316e62daa23
VimanyuAgg <vimanyu1022007@gmail.com>
2017-04-01 17:26:26 -0700

    Add conditions to parse for class as well as sequence diagrams in main()

--- src/main/java/cmpe202/umlparser/App.java
+++ src/main/java/cmpe202/umlparser/App.java
@@ -11,12 +11,17 @@ class App{
 			System.out.println("Error: please enter valid arguments");
 			System.exit(0);
 		}
-		else if (args[0].equals(("sqnc")))
-		{parser myUmlParser = new parser(args[1],args[2],args[3], args[4]);
+
+		else if(args[0].equals("cls")){
+			parser myUmlParser = new parser(args[1],args[2]);
+			myUmlParser.startParsing();
+		}
+		else if (args[0].equals("sqnc"))
+		{SeqDiagramGenerator myUmlParser = new SeqDiagramGenerator(args[1],args[2],args[3], args[4]);
 			try {
 				myUmlParser.startParsing();
 			} catch (Exception e) {
-
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
