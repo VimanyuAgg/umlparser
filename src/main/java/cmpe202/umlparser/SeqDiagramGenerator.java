69515d9c63ff188be087dc2df840fb1d76562404
VimanyuAgg <vimanyu1022007@gmail.com>
2017-04-08 17:54:10 -0700

    write logic for startParsing() seq diagram

--- src/main/java/cmpe202/umlparser/SeqDiagramGenerator.java
+++ src/main/java/cmpe202/umlparser/SeqDiagramGenerator.java
@@ -20,6 +20,9 @@ class SeqDiagramGenerator{
     String className;      //Holds the class Name of the source file
     String methodName;     //Holds the method Name of the source file
 
+    ArrayList<CompilationUnit> compilationArray;
+    String plantUml;
+
     SeqDiagramGenerator(String sourceFilePath, String destFilePath, String className, String methodName){
         this.sourceFilePath = sourceFilePath;
         this.destFilePath = sourceFilePath + "\\" + destFilePath + ".jpeg";
@@ -31,9 +34,38 @@ class SeqDiagramGenerator{
     public void startParsing() throws Exception{
 
         //Dummy method created
+        compilationArray = getArray(sourceFilePath);
+            createMappings();
+            plantUml += "actor user #black\n";
+            plantUml += "user" + " -> " + className + " : " + methodName + "\n";
+
+            parsingHandler(methodName);
+            plantUml += "@enduml";
+            //generateDiagram(plantUml);
+            System.out.println("Plant UML Code:\n" + plantUml);
 
 
 
+    }
+
+    private ArrayList<CompilationUnit> getArray(String inPath)
+            throws Exception {
+
+     File folder = new File(inPath);
+        ArrayList<CompilationUnit> compilationArray = new ArrayList<CompilationUnit>();
+        for (final File file : folder.listFiles()) {
+            if (file.isFile() && file.getName().endsWith(".java")) {
+                FileInputStream inStream = new FileInputStream(file);
+                CompilationUnit cu;
+                try {
+                    cu = JavaParser.parse(inStream);
+                    compilationArray.add(cu);
+                } finally {
+                    inStream.close();
+                }
+            }
+        }
+        return compilationArray;
     }
 
 }
