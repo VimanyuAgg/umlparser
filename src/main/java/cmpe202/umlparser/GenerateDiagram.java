d52c3888798a3fa6fe26a1fe84485f85debba914
VimanyuAgg <vimanyu1022007@gmail.com>
2017-04-22 15:33:08 -0700

    Add GenerateDiagram class with png generation

---
+++ src/main/java/cmpe202/umlparser/GenerateDiagram.java
@@ -0,0 +1,28 @@
+package cmpe202.umlparser;
+
+public class GenerateDiagram {
+    public static Boolean generatePNG(String grammar, String outPath) {
+
+            String webLink = "http://yuml.me/diagram/plain/class/" + grammar
+                    + ".png";
+            URL url = new URL(webLink);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+
+            OutputStream outputStream = new FileOutputStream(new File(outPath));
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            while ((read = conn.getInputStream().read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+            outputStream.close();
+            conn.disconnect();
+
+
+        return null;
+    }
+
+}
